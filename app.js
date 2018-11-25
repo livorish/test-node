649a10e5fc59bd4b2065fc7e6688ebb7745be6e4
livorish <hoksini@gmail.com>
2018-11-25 08:37:25 +0300

    second  commit

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ http.createServer(function(request,response){
     response.end("Hello NodeJS!");
      
 }).listen(3000, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
+    console.log("Сервер начал прослушивание запросов на порту 3000 The best");
 });
